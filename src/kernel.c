18dca64e8d19dfd1fff1f547b8d59daa5dd0d731
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-02-03 21:59:53 -0600

    Please have a bootable kernel...

--- src/kernel.c
+++ src/kernel.c
@@ -1,23 +1,21 @@
-#if defined(__linux__)
-	#error "Use a cross compiler!"
+#if !defined(__cplusplus)
+#include <stdbool.h> /* C doesn't have booleans by default. */
 #endif
-
-#if !defined(__i386__)
-	#error "Use ix86-elf compiler!"
-#endif
-
-#include <stdint.h>
 #include <stddef.h>
-
-static const unsigned int SCREEN_WIDTH = 80;
-static const unsigned int SCREEN_HEIGHT = 25;
-
-unsigned int TERMINAL_ROW;
-unsigned int TERMINAL_COLUMN;
-uint8_t TERMINAL_COLOR;
-uint16_t* TERMINAL_BUFFER;
-
-enum SCREEN_COLOR
+#include <stdint.h>
+ 
+/* Check if the compiler thinks if we are targeting the wrong operating system. */
+#if defined(__linux__)
+#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#endif
+ 
+/* This tutorial will only work for the 32-bit ix86 targets. */
+#if !defined(__i386__)
+#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#endif
+ 
+/* Hardware text mode color constants. */
+enum vga_color
 {
 	COLOR_BLACK = 0,
 	COLOR_BLUE = 1,
@@ -34,92 +32,91 @@ enum SCREEN_COLOR
 	COLOR_LIGHT_RED = 12,
 	COLOR_LIGHT_MAGENTA = 13,
 	COLOR_LIGHT_BROWN = 14,
-	COLOR_WHITE = 15
+	COLOR_WHITE = 15,
 };
-
-uint8_t create_color(enum SCREEN_COLOR fore, enum SCREEN_COLOR back)
+ 
+uint8_t make_color(enum vga_color fg, enum vga_color bg)
 {
-	return fore | back << 4;
+	return fg | bg << 4;
 }
-
-uint8_t create_character(char c, uint8_t color)
+ 
+uint16_t make_vgaentry(char c, uint8_t color)
 {
-	uint16_t ch16 = c;
+	uint16_t c16 = c;
 	uint16_t color16 = color;
-	
-	return ch16 | color16 << 8;
+	return c16 | color16 << 8;
 }
-
-unsigned int strlen(const char* str)
+ 
+size_t strlen(const char* str)
 {
-	unsigned int size = 0;
-	while(str[size] != 0)
-		size++;
-		
-	return size;
+	size_t ret = 0;
+	while ( str[ret] != 0 )
+		ret++;
+	return ret;
 }
-
-// Fill the screen with the correct color...
+ 
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+ 
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+ 
 void terminal_initialize()
 {
-	TERMINAL_ROW = 0;
-	TERMINAL_COLUMN = 0;
-	TERMINAL_COLOR = create_color(COLOR_CYAN, COLOR_RED);
-	// Begin the buffer at the start of 
-	// VGA Video Memory
-	TERMINAL_BUFFER = (uint16_t*) 0xB8000; 
-	
-	for(int x = 0; x < SCREEN_WIDTH; x++)
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
 	{
-		for(int y = 0; y < SCREEN_HEIGHT; y++)
+		for ( size_t x = 0; x < VGA_WIDTH; x++ )
 		{
-			// Index is the spot in memory the character gets
-			// written to
-			const unsigned int index = y * SCREEN_WIDTH + x;  
-			TERMINAL_BUFFER[index] = create_character(' ', TERMINAL_COLOR);
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
 		}
 	}
 }
-
-void terminal_set_color(uint8_t color)
+ 
+void terminal_setcolor(uint8_t color)
 {
-	TERMINAL_COLOR = color;
+	terminal_color = color;
 }
-
-void terminal_make_entry(char c, uint8_t color, unsigned int x, unsigned int y)
+ 
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
-		const unsigned int index = y * SCREEN_WIDTH + x;
-		TERMINAL_BUFFER[index] = create_character(c, color);
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = make_vgaentry(c, color);
 }
-
-void terminal_put_char(char c)
+ 
+void terminal_putchar(char c)
 {
-	terminal_make_entry(c, TERMINAL_COLOR, TERMINAL_COLUMN, TERMINAL_ROW);
-	
-	if(++TERMINAL_COLUMN == SCREEN_WIDTH)
+	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	if ( ++terminal_column == VGA_WIDTH )
 	{
-		TERMINAL_COLUMN = 0;
-		
-		if(++TERMINAL_ROW == SCREEN_HEIGHT)
+		terminal_column = 0;
+		if ( ++terminal_row == VGA_HEIGHT )
 		{
-			TERMINAL_ROW = 0;
+			terminal_row = 0;
 		}
 	}
 }
-
-void terminal_write(const char* str)
+ 
+void terminal_writestring(const char* data)
 {
-	unsigned int length = strlen(str);
-	
-	for(unsigned int i = 0; i < length; i++)
-		terminal_put_char(str[i]);
+	size_t datalen = strlen(data);
+	for ( size_t i = 0; i < datalen; i++ )
+		terminal_putchar(data[i]);
 }
-
+ 
 #if defined(__cplusplus)
-extern "C"
+extern "C" /* Use C linkage for kernel_main. */
 #endif
-int main(struct multiboot *mboot_pointer)
+void kernel_main()
 {
 	terminal_initialize();
-	terminal_write("Hello, and welcome to BrenOS!");
+	/* Since there is no support for newlines in terminal_putchar yet, \n will
+	   produce some VGA specific character instead. This is normal. */
+	terminal_writestring("Hello, kernel World!\n");
 }
